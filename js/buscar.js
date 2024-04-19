0b74869aef8abb8b9c1de6eddd67f1ef6d743ec7
Lee <davi.soo.chan.lee@accenture.com>
2024-04-18 21:29:45 -0300

    dia18.04

--- js/buscar.js
+++ js/buscar.js
@@ -7,10 +7,36 @@ async function buscar() {
 
     const cards = document.getElementById("cards_games");
 
-    cards.innerHTML += respostaDadosAgenda.map((Agenda) => {
+    cards.innerHTML += respostaDadosAgenda.map((itemAgenda) => {
         // porque que é itemAgenda?
-        return "Teste: "
-    });
+        return `
+        <div class="cardItem">
+        <div class="dataGame"> 
+            <img src="../imagens/calendar-solid.svg" alt=""> 20/04 21:30 
+        </div>
+        <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+        <div class="descricao">
+            <p> <strong>${itemAgenda.nome}</strong> </p>
+            <p> Descrição de como vai funcionar o GamePlay, frase legal para chamar a galera :D </p>
+            <p><strong>Gamers: </strong></p>
+
+            <div class="gamers">
+               ${
+                itemAgenda.gamers.map((gamerItem)=> {
+                    return `<div class="gamerItem">@${gamerItem}</div>`
+                }).join('')
+               }
+            
+            </div>
+
+          
+        </div>  
+        <div class="assistir">
+                <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+    </div>
+        `
+    }).join('');
 }
 
 console.log("antes de buscar")
